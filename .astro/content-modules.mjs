7c63e9016365fe907efaaf585a29ffdcf4c11066
Emerson Kirby <11035307+emersonk@users.noreply.github.com>
2026-06-15 16:02:49 -0700

    Change of wording

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,17 +1,10 @@
 
 export default new Map([
-["src/content/projects/3-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-lightheart.mdx&astroContentModuleFlag=true")],
-["src/content/projects/1-gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-gobsmacked.mdx&astroContentModuleFlag=true")],
-["src/content/projects/2-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-pigeon.mdx&astroContentModuleFlag=true")],
-["src/content/projects/4-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-miireader.mdx&astroContentModuleFlag=true")],
-["src/content/projects/gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fgobsmacked.mdx&astroContentModuleFlag=true")],
-["src/content/projects/pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fpigeon.mdx&astroContentModuleFlag=true")],
-["src/content/projects/miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fmiireader.mdx&astroContentModuleFlag=true")],
-["src/content/projects/lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Flightheart.mdx&astroContentModuleFlag=true")],
-["src/content/projects/zgobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fzgobsmacked.mdx&astroContentModuleFlag=true")],
+["src/content/projects/1-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-pigeon.mdx&astroContentModuleFlag=true")],
+["src/content/projects/2-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-lightheart.mdx&astroContentModuleFlag=true")],
+["src/content/projects/3-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-miireader.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/1-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-lightheart.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/2-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-miireader.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/3-gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-gobsmacked.mdx&astroContentModuleFlag=true")],
-["src/content/projects/4-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-pigeon.mdx&astroContentModuleFlag=true")],
-["src/content/projects/1-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-miireader.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/4-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-pigeon.mdx&astroContentModuleFlag=true")]]);
 		
