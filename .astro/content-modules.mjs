7fbf1e1563fe9c0ef88fedca43434a81a61eb86d
Emerson Kirby <11035307+emersonk@users.noreply.github.com>
2026-06-15 14:47:26 -0700

    Updates

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/projects/3-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-lightheart.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/1-gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-gobsmacked.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/2-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-pigeon.mdx&astroContentModuleFlag=true")],
+["src/content/projects/3-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-lightheart.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/4-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-miireader.mdx&astroContentModuleFlag=true")]]);
 		
