5a0701da9022662efde2b84b06a5b41769f66bbc
Emerson Kirby <11035307+emersonk@users.noreply.github.com>
2026-06-15 21:32:39 -0700

    Update css

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,7 @@
 
 export default new Map([
-["src/content/projects/1-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-pigeon.mdx&astroContentModuleFlag=true")],
-["src/content/projects/2-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-lightheart.mdx&astroContentModuleFlag=true")],
-["src/content/projects/3-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-miireader.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/1-lightheart.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F1-lightheart.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/2-miireader.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F2-miireader.mdx&astroContentModuleFlag=true")],
-["src/content/projects/3-gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-gobsmacked.mdx&astroContentModuleFlag=true")],
-["src/content/projects/4-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-pigeon.mdx&astroContentModuleFlag=true")]]);
+["src/content/projects/4-pigeon.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F4-pigeon.mdx&astroContentModuleFlag=true")],
+["src/content/projects/3-gobsmacked.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2F3-gobsmacked.mdx&astroContentModuleFlag=true")]]);
 		
